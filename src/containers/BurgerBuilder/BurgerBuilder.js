2bba73fd6f4b3aa1b240e422645ac802071f2117
Kiko Seijo <kiko@sunnyface.com>
2017-10-29 01:52:28 +0200

    Lecture 118;

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -35,14 +35,34 @@ class BurgerBuilder extends Component {
     this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
   }
   removeIngredientHandler = (type) => {
-
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0){
+      return;
+    }
+    const updatedCounted = oldCount - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    }
+    updatedIngredients[type] = updatedCounted;
+    const priceAddition = INGREDIENTS_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceAddition;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
   }
 
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disabledInfo){
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
     return (
         <Aux>
             <Burger ingredients={this.state.ingredients} />
             <BuildControls
+              disabled={disabledInfo}
+              ingredientRemoved={this.removeIngredientHandler}
               ingredientAdded={this.addIngredientHandler}/>
         </Aux>);
   }

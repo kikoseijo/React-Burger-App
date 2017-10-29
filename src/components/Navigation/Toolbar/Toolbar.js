b8975c98bb5224b2210871f34489d5e5e66c4a13
Kiko Seijo <kiko@sunnyface.com>
2017-10-29 02:47:50 +0100

    Lesson 128

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
-
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
         <Logo />
         <nav>
-            <ul>
-                <li>Menu 1</li>
-                <li>Menu 1</li>
-                <li>Menu 1</li>
-            </ul>
+            <NavigationItems />
         </nav>
     </header>
 );

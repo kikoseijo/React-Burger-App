02cd664c8eed222c48afa6adde0d70e88deb96d7
Kiko Seijo <kiko@sunnyface.com>
2017-10-29 23:20:46 +0100

    Redux - Installing remote dev tool for Redux. 2 lines of code.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import devToolsEnhancer from 'remote-redux-devtools';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+const store = createStore(reducer, devToolsEnhancer());
 
 const app = (
     <Provider store={store}>
